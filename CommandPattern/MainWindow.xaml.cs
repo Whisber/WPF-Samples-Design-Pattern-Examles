0f0fc2e78f59dc271674a50e08c265a96b773224
Whisber <Whisber1@gmail.com>
2022-06-26 04:06:51 +0300

    MainWindow cs icine Command Pattern uygulandı

--- CommandPattern/MainWindow.xaml.cs
+++ CommandPattern/MainWindow.xaml.cs
@@ -24,52 +24,141 @@ namespace FormattedTextExample
         {
             InitializeComponent();
             this.Loaded += MainWindow_Loaded;
+
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {            
+        {
+
             DrawFormattedText(VisualTreeHelper.GetDpi(this));
         }
 
-        protected override void OnDpiChanged(DpiScale oldDpiScaleInfo, DpiScale newDpiScaleInfo)
+        private void OnDpiChanged(DpiScale oldDpiScaleInfo, DpiScale newDpiScaleInfo)
         {
+
             DrawFormattedText(newDpiScaleInfo);
         }
-
         private void DrawFormattedText(DpiScale dpiInfo)
         {
-            FormattedText formattedText = new FormattedText(
-                "FABLE",
-                new System.Globalization.CultureInfo("en-US"),
-                FlowDirection.LeftToRight,
-                new Typeface(
-                    new System.Windows.Media.FontFamily("Segoe UI"),
-                    FontStyles.Normal,
-                    FontWeights.Bold,
-                    FontStretches.Normal),
-                120,
-                System.Windows.Media.Brushes.Red,
-                dpiInfo.PixelsPerDip);
+            CommandClass com = new CommandClass(dpiInfo);
+            Draw drawIt = new Draw(com);
+            MakeBigger big = new MakeBigger(com);
+            drawIt.Execute();
+            big.Execute();
 
-            // Build a geometry out of the text.
-            Geometry geometry = new PathGeometry();
-            geometry = formattedText.BuildGeometry(new System.Windows.Point(0, 0));
+        } /// <summary>
+          /// //////////////////////Burda uygulamaya basladık command ///////////////////////////////
+          /// </summary>
+        public interface Icommand
+        {
+            void Execute();
+        }
+        public class CommandClass : MainWindow, Icommand
+        {
+            DpiScale dpiInfo;
 
-            // Adjust the geometry to fit the aspect ration of the video by scaling it.
-            ScaleTransform myScaleTransform = new ScaleTransform();
-            myScaleTransform.ScaleX = .85;
-            myScaleTransform.ScaleY = 2.0;
-            geometry.Transform = myScaleTransform;
+            public CommandClass(DpiScale dpiInfo)
+            {
+                this.dpiInfo = dpiInfo;
+            }
+            public void DrawFormattedText() //1. fonksiyon
+            {
+                FormattedText formattedText = new FormattedText(
+                    "FABLE",
+                    new System.Globalization.CultureInfo("en-US"),
+                    FlowDirection.LeftToRight,
+                    new Typeface(
+                        new System.Windows.Media.FontFamily("Segoe UI"),
+                        FontStyles.Normal,
+                        FontWeights.Bold,
+                        FontStretches.Normal),
+                    120,
+                    System.Windows.Media.Brushes.Red,
+                    dpiInfo.PixelsPerDip);
 
-            // Flatten the geometry and create a PathGeometry out of it.
-            PathGeometry pathGeometry = new PathGeometry();
-            pathGeometry = geometry.GetFlattenedPathGeometry();
+                // Build a geometry out of the text.
+                Geometry geometry = new PathGeometry();
+                geometry = formattedText.BuildGeometry(new System.Windows.Point(0, 0));
 
-            // Use the PathGeometry for the empty placeholder Path element in XAML.
-            path.Data = pathGeometry;
+                // Adjust the geometry to fit the aspect ration of the video by scaling it.
+                ScaleTransform myScaleTransform = new ScaleTransform();
+                myScaleTransform.ScaleX = .85;
+                myScaleTransform.ScaleY = 2.0;
+                geometry.Transform = myScaleTransform;
 
-            // Use the PathGeometry for the animated ball that follows the path of the text outline.
-            matrixAnimation.PathGeometry = pathGeometry;
+                // Flatten the geometry and create a PathGeometry out of it.
+                PathGeometry pathGeometry = new PathGeometry();
+                pathGeometry = geometry.GetFlattenedPathGeometry();
+
+                // Use the PathGeometry for the empty placeholder Path element in XAML.
+                path.Data = pathGeometry;
+
+                // Use the PathGeometry for the animated ball that follows the path of the text outline.
+                matrixAnimation.PathGeometry = pathGeometry;
+            }
+            public void MakeItBigger() // Burada oluşacak olan fable text i, daha büyük olacak ve rengi "red" yerine "blue" olacak
+            { // 2. fonksiyon
+                FormattedText formattedText = new FormattedText(
+                    "FABLE",
+                    new System.Globalization.CultureInfo("en-US"),
+                    FlowDirection.LeftToRight,
+                    new Typeface(
+                        new System.Windows.Media.FontFamily("Segoe UI"),
+                        FontStyles.Normal,
+                        FontWeights.Bold,
+                        FontStretches.Normal),
+                    180, // Burada büyütme işlemi yapıldı ve renk degisikligi saglandı
+                    System.Windows.Media.Brushes.Blue,
+                    dpiInfo.PixelsPerDip);
+
+                // Build a geometry out of the text.
+                Geometry geometry = new PathGeometry();
+                geometry = formattedText.BuildGeometry(new System.Windows.Point(0, 0));
+
+                // Adjust the geometry to fit the aspect ration of the video by scaling it.
+                ScaleTransform myScaleTransform = new ScaleTransform();
+                myScaleTransform.ScaleX = .85;
+                myScaleTransform.ScaleY = 2.0;
+                geometry.Transform = myScaleTransform;
+
+                // Flatten the geometry and create a PathGeometry out of it.
+                PathGeometry pathGeometry = new PathGeometry();
+                pathGeometry = geometry.GetFlattenedPathGeometry();
+
+                // Use the PathGeometry for the empty placeholder Path element in XAML.
+                path.Data = pathGeometry;
+
+                // Use the PathGeometry for the animated ball that follows the path of the text outline.
+                matrixAnimation.PathGeometry = pathGeometry;
+            }
+            public void Execute()
+            {
+                this.DrawFormattedText();
+            }
+        }
+        public class Draw : Icommand
+        {
+            private CommandClass cc;
+            public Draw(CommandClass cc)
+            {
+                this.cc = cc;
+            }
+            public void Execute()
+            {
+                cc.DrawFormattedText();
+            }
+        }
+        public class MakeBigger : Icommand
+        {
+            private CommandClass cc;
+            public MakeBigger(CommandClass cc)
+            {
+                this.cc = cc;
+            }
+            public void Execute()
+            {
+                cc.MakeItBigger();
+            }
         }
     }
 }
